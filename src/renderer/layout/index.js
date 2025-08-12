247b93ef46d925d86fcb5e39c9434c0c81eac78e
AbdullahAtique80 <abdullah.atique@xcorebit.com>
2025-08-12 10:33:40 +0500

    [SC-38792] - refactoring

--- src/renderer/layout/index.js
+++ src/renderer/layout/index.js
@@ -1,16 +1,3 @@
-// window.electronAPI.onShowAppVersion((version) => {
-//   let element = document.getElementById("app-version");
-
-//   element.innerHTML = `App version: ${version}`;
-// });
-
-// In your renderer JS (e.g., src/renderer/layout/index.js)
-// const { ipcRenderer } = window.require ? window.require('electron') : require('electron');
-
-// ipcRenderer.on("show-app-version", (event, version) => {
-//   document.getElementById("app-version").textContent = version;
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
 
   window.electronAPI.onShowAppVersion((version) => {
@@ -19,4 +6,3 @@ document.addEventListener("DOMContentLoaded", () => {
     element.innerHTML = `App version: ${version}`;
   });
 })
-
