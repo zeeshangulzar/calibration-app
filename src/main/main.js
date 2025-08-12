bc4bc2b88c1dc64c479cd9593bb82bfc5869160c
AbdullahAtique80 <abdullah.atique@xcorebit.com>
2025-08-12 13:02:30 +0500

    [SC-38792] - updates to prettier and main.js

--- src/main/main.js
+++ src/main/main.js
@@ -1,11 +1,11 @@
-import EventEmitter from "events";
+import EventEmitter from 'events';
 EventEmitter.defaultMaxListeners = 500;
-import { app } from "electron";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import { createMainWindow } from "./windows/main.js";
-import { registerIpcHandlers } from "./ipc/index.js";
+import { app } from 'electron';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createMainWindow } from './windows/main.js';
+import { registerIpcHandlers } from './ipc/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,8 +14,8 @@ dotenv.config();
 
 const currentAppVersion = app.getVersion();
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
